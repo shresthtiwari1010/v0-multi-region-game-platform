73a0f88f8a8f86e38a05b8d89b2781eb22fd7626
Shresth Tiwari <shresthtiwari1010@gmail.com>
2026-03-16 20:19:12 +0530

    DocuVerse Auto-Fix: 4 file(s) improved (#2)
    * fix: DocuVerse auto-fix for components/matchmaking-criteria.tsx
    
    * fix: DocuVerse auto-fix for components/discord-integration.tsx
    
    * fix: DocuVerse auto-fix for components/theme-provider.tsx
    
    * fix: DocuVerse auto-fix for lib/utils.ts

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,20 @@
+```
 'use client'
 
-import * as React from 'react'
+import * as React from'react'
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
 } from 'next-themes'
+import { useSharedUtilities } from '@lib/utilities'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children,...props }: ThemeProviderProps) {
+  const { ensureThemeConsistency } = useSharedUtilities()
+
+  return (
+    <NextThemesProvider {...props} value={ensureThemeConsistency(props.value)}>
+      {children}
+    </NextThemesProvider>
+  )
 }
+```
